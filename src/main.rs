9ec91956994193f5e38af6e47a0eb688f5c034e0
ltylty <1137588860@qq.com>
2026-06-23 20:21:11 +0800

    refactor: update test runner to use Testable trait for improved test execution

--- src/main.rs
+++ src/main.rs
@@ -31,10 +31,10 @@ pub extern "C" fn _start() -> ! {
 
 
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
+pub fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
 }
 
@@ -47,7 +47,22 @@ fn trivial_assertion() {
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
+    // serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    serial_println!("[ok]");
+    // serial_println!("[ok]");
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
 }

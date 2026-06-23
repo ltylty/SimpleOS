c8c315b681d74eb6e29e70333f2d078ace483c2f
ltylty <1137588860@qq.com>
2026-06-23 16:11:18 +0800

    rust

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![no_std] // 不链接 Rust 标准库
+#![no_main] // 禁用所有 Rust 层级的入口点
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+/// 这个函数将在 panic 时被调用
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
+    loop {}
+}
+
